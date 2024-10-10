308278da6d7e5bf9a44134ee5ff6d7f417b966a9
AnwarWaxir <anwarhaali1@gmail.com>
2024-10-10 09:28:07 +0500

    complete calculate_btn function, here I use this formula = w / (h * h) and use toFixed method to allow only 2 digits after the number

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var weight = document.getElementById('weight-input')
-var height = document.getElementById('height-input')
-var result = document.getElementById('result')
-var messege = document.getElementById('messege')
-
 document.getElementById('cal_btn').addEventListener('click', function(){
-    
+    var weight = document.getElementById('weight-input').value;
+    var height = document.getElementById('height-input').value;
+    var final_result = (weight / (height * height)).toFixed(2)
+    document.getElementById('result').textContent = final_result;
 })
