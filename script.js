5a925ce5cb94c1335e84fabb0eddd09910e3dc29
AnwarWaxir <anwarhaali1@gmail.com>
2024-10-10 08:20:02 +0500

    add function to cal_btn and reset_btn

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ var weight = document.getElementById('weight-input')
 var height = document.getElementById('height-input')
 var result = document.getElementById('result')
 var messege = document.getElementById('messege')
+
+document.getElementById('cal_btn').addEventListener('click', function(){
+    
+})
