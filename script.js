6897291ac670cf2377e9734e358c22dbc08267fa
AnwarWaxir <anwarhaali1@gmail.com>
2024-10-10 10:53:50 +0500

    add switch statements to cal_btn to show result messeges and functionalized reset_btn function and set it to empty input fields when it filled

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 document.getElementById('cal_btn').addEventListener('click', function(){
     var weight = document.getElementById('weight-input').value;
-    var height = document.getElementById('height-input').value;
-    var final_result = (weight / (height * height)).toFixed(2)
-    document.getElementById('result').textContent = final_result;
+    var height_feet = document.getElementById('height-input').value;
+    var height = height_feet * 0.3048;
+    var bmi = (weight / (height * height)).toFixed(2)
+    document.getElementById('result').textContent = bmi;
+    var message = document.getElementById('message')
+
+    switch(true){
+        case bmi < 18.5:
+            message.textContent = 'Underweight';
+            break;
+
+        case bmi >= 18.5 && bmi <= 24.9:
+            message.textContent = 'Normail Weight';
+            break;
+
+        case bmi >= 25 && bmi <= 29.9:
+            message.textContent = 'Overweight';
+            break;
+
+        case bmi => 30:
+            message.textContent = 'Obesity';
+            break;
+
+        default:
+            message.textContent = 'Your entered invalid value';
+    }
+})
+
+document.getElementById('reset_btn').addEventListener('click', function(){
+    var weight = document.getElementById('weight-input');
+    var height = document.getElementById('height-input');
+    height.value = ''
+    weight.value = ''
 })
