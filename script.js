c9b47534015e2ecb0b0447fd8c2e7288c626b0a5
AnwarWaxir <anwarhaali1@gmail.com>
2024-10-10 22:29:31 +0500

    add two if conditions to check if the values are 0 or negative so show alert and stop code execution

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 document.getElementById('cal_btn').addEventListener('click', function(){
     var weight = document.getElementById('weight-input').value;
-    var height_feet = document.getElementById('height-input').value;
-    var height = height_feet * 0.3048;
-    var bmi = (weight / (height * height)).toFixed(2)
+    var height = document.getElementById('height-input').value;
+    var bmi = (weight / (height * height)).toFixed(2) // Use BMI formula to find bmi and allow 2 digits after the decimals
     var result = document.getElementById('result')
-    result.textContent = bmi;
+    result.textContent = bmi; // Put bmi values to result element
     var message = document.getElementById('message')
 
-    if(weight == '' && height == '') {
+    if(weight == '' && height == '') {  // If the input fieds are empty so display alert and show "plz enter a valid values"
         alert('Please enter a valid values ...')
         result.textContent = ''
         message.textContent = ''
         return;
     }
 
-    switch(true){
+    if (weight <= 0) {
+        alert("Weight must be a positive number greater than 0.");
+        return;
+    }
+    
+    if (height <= 0) {
+        alert("Height must be a positive number greater than 0.");
+        return;
+    }
+
+    switch(true){  // Write switch case to show result messege means he/she are healthy or not
         case bmi < 18.5:
             message.textContent = 'Underweight';
             break;
@@ -36,7 +45,7 @@ document.getElementById('cal_btn').addEventListener('click', function(){
     }
 })
 
-document.getElementById('reset_btn').addEventListener('click', function(){
+document.getElementById('reset_btn').addEventListener('click', function(){  // This funtion is used for to empty input fieds after we find values and want to add more values
     var weight = document.getElementById('weight-input');
     var height = document.getElementById('height-input');
     height.value = ''
